b4211c9992b1ac40f1422846f401e8d02ce234e9
vazome <danielvazome@outlook.com>
2025-07-04 12:51:14 +0400

    Refactor Airflow secrets in .env, enhance logging in new_simple.py, and update ownership commands in docker-compose.yaml

--- 03-orchestration/dags/new_simple.py
+++ 03-orchestration/dags/new_simple.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import logging
+
 from airflow.sdk import dag, task
 
 
@@ -7,6 +9,8 @@ from airflow.sdk import dag, task
 def new_simple_dag():
     @task
     def my_task():
+        log = logging.getLogger("airflow.task")
+        log.info("Reading data")
         pass
 
     my_task()
