c4237a220b7fbfd493c84bb3083db3b284387ecf
vazome <danielvazome@outlook.com>
2025-07-29 13:24:59 +0400

    Added README and proper duration testing in for batch.

---
+++ 06-best-practices/README.md
@@ -0,0 +1,53 @@
+# Q1. Prepare the dataset
+**What is the shape of the downloaded data? How many rows are there?**
+
+Source: [06-best-practices/batch.py](batch.py)
+
+![alt text](assets/image.png)
+
+# Q2. Installing pytest
+Source: [../pyproject.toml](../pyproject.toml)
+
+![alt text](assets/image-1.png)
+
+**WWhat should be the other file?**
+
+`__init__.py`
+
+# Q3. Writing first unit test
+**How many rows should be there in the expected dataframe?**
+
+Source: [06-best-practices/tests/test_batch.py](tests/test_batch.py)
+
+`2`
+
+![alt text](assets/image-2.png)
+
+# Q4. Mocking S3 with Localstack
+**In both cases we should adjust commands for localstack. What option do we need to use for such purposes?**
+
+> [!NOTE]  
+> During testing my existing AWS SSO configuration was interfereing with LocalStack, I fixed it later, but in screenshot you can see me using mock env vars in terminal.
+
+`--endpoint-url`
+
+![alt text](assets/image-3.png)
+
+
+# Q5. Creating test data
+**What's the size of the file?**
+
+Source: [06-best-practices/integration_test.py](integration_test.py)
+
+`3215` - closest answer is `3620`
+
+![alt text](assets/image-4.png)
+
+# Q6. Finish the integration test
+**What's the sum of predicted durations for the test dataframe?**
+
+Source: [06-best-practices/integration_test.py](integration_test.py)
+
+36.28
+
+![alt text](assets/image-5.png)
--- 06-best-practices/tests/test_batch.py
+++ 06-best-practices/tests/test_batch.py
@@ -13,27 +13,32 @@ def test_prepare_data():
         (None, None, dt(1, 1), dt(1, 10)),
         (1, 1, dt(1, 2), dt(1, 10)),
         (1, None, dt(1, 2, 0), dt(1, 2, 59)),
-        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
     ]
 
-    columns = ["PULocationID", "DOLocationID", "tpep_pickup_datetime", "tpep_dropoff_datetime"]
+    columns = [
+        "PULocationID",
+        "DOLocationID",
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime",
+    ]
     df = pd.DataFrame(data, columns=columns)
     categorical = ["PULocationID", "DOLocationID"]
-    
+
     actual_result = prepare_data(df, categorical)
-    
+
     expected_data = [
-        ("-1", "-1", 9.0),
-        ("1", "1", 8.0),
+        ("-1", "-1", dt(1, 1), dt(1, 10), 9.0),
+        ("1", "1", dt(1, 2), dt(1, 10), 8.0),
     ]
-    expected_columns = ["PULocationID", "DOLocationID", "duration"]
-    expected_result = pd.DataFrame(expected_data, columns=expected_columns)
-    
-    actual_dicts = actual_result[expected_columns].to_dict("records")
-    expected_dicts = expected_result.to_dict("records")
-    
-    assert len(actual_dicts) == len(expected_dicts)
-    
-    for actual, expected in zip(actual_dicts, expected_dicts):
-        assert actual["PULocationID"] == expected["PULocationID"]
-        assert actual["DOLocationID"] == expected["DOLocationID"]
+    expected_result = pd.DataFrame(expected_data, columns=columns + ["duration"])
+    expected_result = expected_result.astype(
+        {"PULocationID": "object", "DOLocationID": "object"}
+    )
+
+    print("Expected:")
+    print(expected_result)
+    print("\nActual:")
+    print(actual_result)
+
+    pd.testing.assert_frame_equal(actual_result, expected_result)
